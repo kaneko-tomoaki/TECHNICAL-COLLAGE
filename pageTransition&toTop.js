ec50c7cab6b18052ccef831333088ef3268ba1f6
kaneko-tomoaki <burg.kaneko@gmail.com>
2025-11-10 15:39:30 +0900

    1112配布

---
+++ pageTransition&toTop.js
@@ -0,0 +1,42 @@
+// ここからページ遷移時のフェードインを設定
+
+window.addEventListener('DOMContentLoaded', () => {   // DOMの読み込み終了後に実行
+  document.body.style.opacity = 0;                    // ページ表示直後は透明な状態のままで
+  document.body.style.transition = 'opacity 0.8s';    // 透明度の変化（フェード）を0.8秒かけて行う
+  setTimeout(() => {                                  // 少しだけ遅延させ透明化をスタート（フェードイン開始）
+    document.body.style.opacity = 1;
+  }, 10);
+}); // フェードイン効果ここまで
+
+// ここからはページ遷移時のフェードアウトを設定
+
+  document.addEventListener('DOMContentLoaded', () => { // DOMの読み込み終了後に実行
+    document.querySelectorAll('a').forEach(link => {    // すべてのaタグ（リンク）を網羅しつつ処理する
+      if (
+        link.target === '_blank' ||
+        link.href.startsWith('javascript') ||
+        link.getAttribute('href').startsWith('#')
+      ) return;
+      link.addEventListener('click', function(e) {      // リンクがクリックされた際の処理を追加
+        e.preventDefault();                             // デフォルトのリンク動作（即遷移）はストップし、
+        document.body.style.opacity = 0;                // フェードアウト（ページを徐々に透明にして）
+        setTimeout(() => {                              // 0.8秒後に次のリンク先へ遷移していく
+          window.location.href = this.href;
+        }, 800);
+      });
+    });
+  }); // フェードアウト効果ここまで
+
+// ページトップへ戻る（スムーズスクロール）
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelectorAll('a[href^="#"]').forEach(link => {
+    link.addEventListener('click', function(e) {
+      const href = this.getAttribute('href');
+      // hrefが「#」または「#top」など、かつ要素が存在する場合のみスムーズスクロール
+      if (href.length > 1 && document.querySelector(href)) {
+        e.preventDefault();
+        document.querySelector(href).scrollIntoView({ behavior: 'smooth' });
+      }
+    });
+  });
+});
